07f36a2c72daa200a39a19f51552936bf768c9e2
Shahrukh <khan_shahrukh89@yahoo.com>
2018-12-17 00:17:47 +0530

    Homework #1

---
+++ main.py
@@ -0,0 +1,33 @@
+# Set title of the song
+title = "Summer of 69"
+# Set release date of the song
+release_date = "17th of June 1985"
+# Set format of the song
+format = "Phonograph record"
+# Set recorded year
+recorded = 1984
+# Set song genre
+genre = "rock, pop rock"
+# Set song length
+length = 3.32
+# Set song label
+label = "A&M"
+# Set song writers
+song_writers = "Bryan Adams, Jim Vallance"
+# Set song producers
+producers = "Bob Clearmountain"
+
+'''
+should print all the variable that we have set above
+'''
+
+print("Title: " + title)
+print("Release date: " + release_date)
+print("Format: "+ format)
+print("Recorded: " + str(recorded))
+print("Genre: " + genre)
+print("Length: " + str(length))
+print("Label: " + label)
+print("Song writers: " + song_writers)
+print("Producers: " + producers)
+
